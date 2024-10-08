03be53efb392b0f09437f511ac40994e4535cddc
Ray <panray@163.com>
2024-10-08 23:25:21 +0800

    reciv

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,28 @@
 #include <esp_now.h>
 
 // 设置数据结构体
-typedef struct struct_message {
-  double shuzi;
+typedef struct struct_message
+{
+  String call;
+  double data;
 } struct_message;
 
-struct_message myData;
 
+struct_message myData;
+uint8_t UpAddrees;//上位机地址
+//uint8_t UpAddrees;
+String theCall;
 //esp_now_recv_info my_recv_info;
 // 数据接收回调函数
 void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int len) {
-  //char macStr[18];
-  //Serial.print("Packet from: ");
-  //const uint8_t * mac_addr=my_recv_info.src_addr;
- // snprintf(macStr, sizeof(my_recv_info.src_addr), "%02x:%02x:%02x:%02x:%02x:%02x",
-    //       my_recv_info.src_addr[0], my_recv_info.src_addr[1], my_recv_info.src_addr[2], my_recv_info.src_addr[3], my_recv_info.src_addr[4], my_recv_info.src_addr[5]);
-  //Serial.println(macStr);
-
+  char macStr[18];
+  Serial.print("Packet from: ");
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+   mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.println(macStr);
+  UpAddrees=*mac_addr;
   memcpy(&myData, data, sizeof(myData));
-  Serial.print("shuzi:");
-  Serial.println(myData.shuzi);
-  Serial.println();
+  theCall=myData.call;
 }
 
 void setup() {
@@ -38,6 +40,26 @@ void setup() {
   // 设置接收数据回调函数
   esp_now_register_recv_cb(OnDataRecv);
 }
-
+float dataT=20.00;
 void loop() {
+  if(theCall=="17860715233"){
+    myData.call="OK!";
+    Serial.println(theCall);
+    esp_err_t result = esp_now_send(&UpAddrees, (uint8_t *)&myData, sizeof(myData));
+  // 检查数据是否发送成功
+  if (result == ESP_OK)
+  {
+    Serial.println("Sent with success");
+  }
+  else
+  {
+    Serial.println("Error sending the data");
+  }
+  
+  }
+
+  dataT=dataT+0.01;
+  myData.data=dataT;
+  delay(500);
 }
+
