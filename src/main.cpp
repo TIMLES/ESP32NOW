21d2f1cf0dcc661d9d68819ea84e334911b60bd4
Ray <panray@163.com>
2024-10-12 20:35:40 +0800

    SQLtrans

--- src/main.cpp
+++ src/main.cpp
@@ -1,105 +1,93 @@
+// esp32连接远程数据库并上传
 #include <WiFi.h>
-#include <esp_now.h>
+#include <MySQL_Connection.h>
+#include <MySQL_Cursor.h>
 
-// 设置数据结构体
-typedef struct struct_message
-{
-  String call;
-  double data;
-} struct_message;
+// WiFi 密码与连接
+char ssid[] = "RAY 0893"; // your SSID
+char pass[] = "12345678"; // your SSID Password
 
-struct_message myData; // 数据对象为myData
+IPAddress server_addr(121, 41, 116, 214); // MySQL服务器IP
+char user[] = "panray";                   // MySQL用户名
+char password[] = "WjCNlhdC4c07mDkb";     // MySQL user 登陆密码
 
-// 接收设备的 MAC 地址
-uint8_t OtheAddrees[] = {0x64, 0xE8, 0x33, 0x7E, 0xB3, 0x0C};
-// 本机地址
-uint8_t myAddrees[] = {0x64, 0xE8, 0x33, 0x89, 0xCA, 0xC8};
+WiFiClient client; // Use this for WiFi客户端
+MySQL_Connection conn((Client *)&client);
 
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
-void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int len);
+void transData_SQL();
 void setup()
+
 {
   Serial.begin(9600);
+  delay(10);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-  // 初始化 ESP-NOW
-  WiFi.mode(WIFI_STA);
-  if (esp_now_init() != ESP_OK)
-  { // ESP_OK表示无错误函数返回
-    Serial.println("Error initializing ESP-NOW");
-    return;
-  }
-  Serial.print("ESP32 Board MAC Address:  ");
-  Serial.println(WiFi.macAddress());
-
-  // 设置发送数据回调函数
-  esp_now_register_send_cb(OnDataSent);
-
-  // 绑定数据接收端
-  esp_now_peer_info_t peerInfo;
-  memcpy(peerInfo.peer_addr, OtheAddrees, 6);
-  peerInfo.channel = 0;
-  peerInfo.encrypt = false; // 发送数据是否加密
-
-  // 检查设备是否配对成功
-  if (esp_now_add_peer(&peerInfo) != ESP_OK)
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED)
   {
-    Serial.println("Failed to add peer");
-    return;
+    delay(500);
+    Serial.print(".");
   }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  // End WiFi section
+  delay(100);
+  transData_SQL();
 }
-String AA;bool kk=0; esp_err_t result;
-int i = 1;
+
 void loop()
 {
+     if (Serial.available() > 0)
+    {
+      String AA = Serial.readString();
+      delay(50);
+      if (AA == "17860715233")
+      {
+        Serial.println(AA);
+        transData_SQL();
+      }
+      else{Serial.println("NO!");}
+}}
 
-  if (Serial.available() > 0)
+// 目标
+void transData_SQL()
+{
+  Serial.println("Connecting...");
+
+  if (conn.connect(server_addr, 3306, user, password))
   {
-    String AA = Serial.readString();
-    delay(50);
-    result = esp_now_send(OtheAddrees, (uint8_t *)&myData, sizeof(myData));
-    kk=0;
-      if (result == ESP_OK)
-  {
-    Serial.println("Sent with success");
+    Serial.println("mySQL is OK!");
+    delay(1000);
   }
   else
-  {
-    Serial.println("Error sending the data");
-  }
-  
-  }
-  // 设置要发送的数据
-  myData.call= AA;
-  myData.data = i;
-  
-  delay(500);
-  i++;
-}
+    Serial.println("Connection failed.");
+  delay(100);
 
-// 数据发送回调函数
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
-{
-  char macStr[18];
-  Serial.print("Packet to: ");
-  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
-           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  Serial.println(macStr);
-  Serial.print("Send status: ");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-  Serial.println();
-}
+  // SQL语句，%s为无符号字符串，可使用sprintf()函数插入目标数据，%f为浮点
+  //char SQLdata[1024]; // 要写入mysql的数据，需满足语句格式
+  char *SQLdata=new char[1024];
 
-void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int len) {
-  memcpy(&myData, data, sizeof(myData));
-  if(kk){
-  char macStr[18];
-  Serial.print("Packet from: ");
-  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
-  mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  Serial.println(macStr);
-  Serial.print("状态:");
-  Serial.println(myData.call);
-  kk=1;
+  char SQL[]="INSERT INTO timles.test (temp,humd) VALUES ('0','0')";
+  strcat(SQLdata, SQL) ;//附加字符串
+  char format[] = ",('%f','%f')"; // 格式
+  char formatData[15] ; // 格式
+  
+  for(int i=0;i<5;i++) {
+  sprintf(formatData, format, i, i*2); // sprintf(被合并的值，数据1，数据2)
+  strcat(SQLdata, formatData) ;//附加字符串
   }
-  Serial.println(myData.data);
+
+  Serial.println(SQLdata);
+  MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn); // 创建一个Mysql实例
+  cur_mem->execute(SQLdata);                       // 将采集到的温湿度值插入数据库中
+  Serial.println("读取传感器数据，并写入数据库");
+  delete cur_mem; // 删除mysql实例为下次传输作准备
+  conn.close();   // close the connection
+  Serial.println("Data Saved.");
+  delete[] SQLdata;
 }
