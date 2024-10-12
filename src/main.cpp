1fd2088feb5cbdbc5b15e9cc10b6b67696b9d849
Ray <panray@163.com>
2024-10-12 15:34:01 +0800

    SQL_test

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,88 @@
-
-#include <WiFi.h>                  
+// esp32连接远程数据库并上传
+#include <WiFi.h>
 #include <MySQL_Connection.h>
 #include <MySQL_Cursor.h>
 
+// WiFi 密码与连接
+char ssid[] = "RAY 0893"; // your SSID
+char pass[] = "12345678"; // your SSID Password
 
-IPAddress server_addr(121,41,116,214);  // IP of the MySQL *server* here
-char user[] = "panray";              // MySQL user login username
-char password[] = "WjCNlhdC4c07mDkb";        // MySQL user login password
+IPAddress server_addr(121, 41, 116, 214); // MySQL服务器IP
+char user[] = "panray";                   // MySQL用户名
+char password[] = "WjCNlhdC4c07mDkb";     // MySQL user 登陆密码
 
-// WiFi card example
-char ssid[] = "RAY 0893";    // your SSID
-char pass[] = "12345678";       // your SSID Password
-WiFiClient client;            // Use this for WiFi instead of EthernetClient
+WiFiClient client; // Use this for WiFi客户端
 MySQL_Connection conn((Client *)&client);
 
-void setup() {
-Serial.begin(9600);
-  delay(10);
+void transData_SQL();
+void setup()
 
+{
+  Serial.begin(9600);
+  delay(10);
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  
-  WiFi.begin(ssid, pass);  
-  while (WiFi.status() != WL_CONNECTED) {
+
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
-  Serial.println("WiFi connected");  
+  Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
   // End WiFi section
 
+}
+
+void loop()
+{
+     if (Serial.available() > 0)
+    {
+      String AA = Serial.readString();
+      delay(50);
+      if (AA == "17860715233")
+      {
+        Serial.println(AA);
+        transData_SQL();
+      }
+      else{Serial.println("NO!");}
+}}
+
+// 目标
+void transData_SQL()
+{
   Serial.println("Connecting...");
-  if (conn.connect(server_addr, 3306, user, password)) {
-    Serial.println("OK!");
+
+  if (conn.connect(server_addr, 3306, user, password))
+  {
+    Serial.println("mySQL is OK!");
     delay(1000);
   }
   else
     Serial.println("Connection failed.");
-  delay(2000);  
-  //insert, change database name and values by string and char[]
-  char INSERT_SQL[] = "INSERT INTO timles.test (temp,humd) VALUES ('35','60')";//傳入的值固定為溫度,濕度為35,60
-  MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);  
-  cur_mem->execute(INSERT_SQL);//execute SQL
-  delete cur_mem;
-  conn.close();                  // close the connection
-  Serial.println("Data Saved.");
-}
+  delay(100);
 
-void loop() {
+  // SQL语句，%s为无符号字符串，可使用sprintf()函数插入目标数据，%f为浮点
+  //char SQLdata[1024]; // 要写入mysql的数据，需满足语句格式
+  char SQLdata[1024];
+  char SQL[]="INSERT INTO timles.test (temp,humd) VALUES";
+  strcat(SQLdata, SQL) ;//附加字符串
+  char format[] = " ('%f','%f')"; // 格式
+  char formatData[15] ; // 格式
+
+  sprintf(formatData, format, 11.1, 2.12); // sprintf(被合并的值，数据1，数据2)
+  strcat(SQLdata, formatData) ;//附加字符串
+
+  Serial.println(SQLdata);
+  MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn); // 创建一个Mysql实例
+  cur_mem->execute(SQLdata);                       // 将采集到的温湿度值插入数据库中
+  Serial.println("读取传感器数据，并写入数据库");
+  delete cur_mem; // 删除mysql实例为下次传输作准备
+  conn.close();   // close the connection
+  Serial.println("Data Saved.");
 }
